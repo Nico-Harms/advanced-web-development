89402366c8e41322b71c06ae84be484ca8e47af2
madsnbkristensen <madskristensen@hotmail.dk>
2023-11-22 00:16:23 +0100

    layer component example

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
     return (
         <main>
             <h2 className="text-3xl font-bold underline text-primary">Home page </h2>
-            <button className="button-primary">Click Me</button>
+            <button className="btn">Click Me</button>
         </main>
     )
 
