f7c12954b34799ee18483f57d93760546e39787f
TobiasRW <tobiasrw98@gmail.com>
2023-11-27 17:46:27 +0100

    ..

--- src/components/semantic/Navigation.jsx
+++ src/components/semantic/Navigation.jsx
@@ -14,13 +14,22 @@ export default function Navigation() {
     }
   }, [location.pathname]);
 
+
   return (
     <>
       {showNavBar && (
-        <nav id="navBar">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+        <nav id="navBar" className="flex bg-off-white justify-between items-center h-14 relative">
+          <div className="font-bebas text-2xl text-prime-orange mx-4">Jumbo Bakery</div>
+          <div id="navLinks" className="flex justify-center gap-8">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+          <div className="hamburger mx-4">
+            <span className="block bg-prime-orange h-1 w-6 rounded m-[2px]"></span>
+            <span className="block bg-prime-orange h-1 w-6 rounded m-[2px]"></span>
+            <span className="block bg-prime-orange h-1 w-6 rounded m-[2px]"></span>
+          </div>
         </nav>
       )}
     </>
