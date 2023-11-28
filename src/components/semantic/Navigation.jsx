0a7f9f1b7ee621c9f8db925136a3ac2bcceb31bb
TobiasRW <tobiasrw98@gmail.com>
2023-11-28 11:34:21 +0100

    header finish

--- src/components/semantic/Navigation.jsx
+++ src/components/semantic/Navigation.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import logo from "../../assets/images/JumboLogo.svg";
 
 export default function Navigation() {
   const [showNavBar, setShowNavBar] = useState(true);
@@ -27,17 +28,17 @@ export default function Navigation() {
   return (
     <>
       {showNavBar && (
-        <nav id="navBar" className="flex bg-off-white justify-between items-center h-14 relative">
-          <div className="font-bebas text-2xl text-prime-orange mx-4">Jumbo Bakery</div>
-          <div id="navLinks" className="flex flex-col h-[100vh] bg-prime-orange pt-20 gap-8 w-[50%] pl-10 text-xl text-off-white ">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+        <nav id="navBar" className="flex bg-background justify-between items-center h-20 relative drop-shadow-lg md:h-28 ">
+          <img src={logo} className=" w-36 mx-4 md:w-52 md:mx-10 "/>
+          <div id="navLinks" className="flex flex-col h-[100vh] bg-prime-orange pt-20 gap-8 w-[50%] pl-10 text-xl text-off-white md:bg-background md:flex-row md:h-auto md:p-0 md:w-auto md:text-off-black md:mx-10 md:gap-20 ">
+            <NavLink to="/" className="link" >Home</NavLink>
+            <NavLink to="/about" className="link" >About</NavLink>
+            <NavLink to="/contact" className="link" >Contact</NavLink>
           </div>
-          <div className="hamburger mx-4 z-10" onClick={toggleBurger}>
-            <span className="block bg-prime-orange h-1 w-6 rounded m-[2px]"></span>
-            <span className="block bg-prime-orange h-1 w-6 rounded m-[2px]"></span>
-            <span className="block bg-prime-orange h-1 w-6 rounded m-[2px]"></span>
+          <div className="hamburger mx-4 z-10 md:hidden" onClick={toggleBurger}>
+            <span className="block bg-prime-orange h-[3px] w-6 rounded m-[2px]"></span>
+            <span className="block bg-prime-orange h-[3px] w-6 rounded m-[2px]"></span>
+            <span className="block bg-prime-orange h-[3px] w-6 rounded m-[2px]"></span>
           </div>
         </nav>
       )}
