6c63a5f0685ae00cd5f64021f0db5e9cff51e978
EmilDalhoff <112545316+EmilDalhoff@users.noreply.github.com>
2023-11-29 11:31:05 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,6 @@
 import HeroSection from "../components/sections/HeroSection";
 import InstaSection from "../components/sections/InstaSection";
 import LocationSection from "../components/sections/LocationSection";
-import Footer from "../components/semantic/Footer"
 import TextSection from "../components/sections/TextSection";
 
 export default function HomePage() {
