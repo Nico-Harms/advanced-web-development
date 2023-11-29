e727885339da6e8529925a34964db0751d42e02c
TobiasRW <tobiasrw98@gmail.com>
2023-11-29 10:53:45 +0100

    text sect done

--- src/components/sections/LocationSection.jsx
+++ src/components/sections/LocationSection.jsx
@@ -3,9 +3,9 @@ import locationIMG from '../../assets/images/location-img.webp'
 
 export default function LocationSection() {
     return (
-        <div className=" bg-prime-brown md:flex md:items-center">
+        <div className=" bg-prime-brown md:flex md:items-center ">
             <div className="flex flex-col gap-9 py-5 md:w-2/4 ">
-                <div className="flex flex-col gap-7 items-center w-[85%] m-auto md:items-start md:mx-20 md:my-2">
+                <div className="flex flex-col gap-7 items-center w-[85%] m-auto md:items-start md:mx-[20%] md:my-2">
                     <div className=" flex flex-col items-center gap-7 md:flex-row md:items-start">
                         <img src={locationSVG} className=' w-10 md:order-last md:w-9 ' alt="location icon" />
                         <h2 className='font-bebas text-off-white text-4xl text-center md:text-6xl'>Lokoationer</h2>
@@ -14,7 +14,7 @@ export default function LocationSection() {
                         <p className='font-mont text-off-white text-sm text-center md:text-base'>Find vores café og bageri i hjertet af Aarhus </p>
                     </div>
                 </div>
-                <div className=" flex gap-5 w-[85%] m-auto md:flex-col md:mx-20 md:my-2">
+                <div className=" flex gap-5 w-[85%] m-auto md:flex-col md:mx-[20%] md:my-2">
                     <div className=" flex flex-col gap-2 items-center w-[50%] md:items-start md:w-auto">
                         <h3 className='font-bebas text-3xl text-off-white md:text-4xl'>Bageri</h3>
                         <p className='font-mont text-off-white text-sm italic md:text-base' >Mindet 4D, 8000 Aarhus C</p>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,13 +3,15 @@ import HeroSection from "../components/sections/HeroSection";
 import InstaSection from "../components/sections/InstaSection";
 import LocationSection from "../components/sections/LocationSection";
 import Footer from "../components/semantic/Footer"
+import TextSection from "../components/sections/TextSection";
+
 export default function HomePage() {
 
     return (
         <main>
             <HeroSection />
             <LocationSection />
-            <Footer />
+            <TextSection />
 
             <InstaSection />
 
