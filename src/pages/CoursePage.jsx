d090df609048aaa09cbbc26bd809ae36dc74a23b
Nicolai Harms <nicolai.harms@gmail.com>
2023-11-30 13:00:32 +0100

    2

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,9 +1,36 @@
+<<<<<<< Updated upstream
 import CourseCard from "../components/CourseCard";
+=======
+import Accordion from "../components/Accordion";
+
+>>>>>>> Stashed changes
 
 export default function ContactPage() {
     return (
         <main>
+<<<<<<< Updated upstream
             <CourseCard />
+=======
+            <div className="h-[70vh] max-w-md mx-auto mt-8">
+      <Accordion
+        title="Parking"
+        content="Lorem ipsum text about the Parking component"
+      />
+      <Accordion
+        title="Ombooking"
+        content="Lorem ipsum text about the Ombooking component"
+        foldIcon="👋"
+      />
+      <Accordion
+        title="Hvad skal du medbringe?"
+        content="Lorem ipsum text about the Hvad skal du medbringe? component"
+      />
+    </div>
+  
+        
+>>>>>>> Stashed changes
         </main>
+
+        
     )
 }
