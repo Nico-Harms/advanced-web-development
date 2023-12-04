2faa0de750483b2a4bfc114c9680a2e1772ae533
madsnbkristensen <madskristensen@hotmail.dk>
2023-12-04 10:12:07 +0100

    firestore

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -2,27 +2,27 @@ import courseIMG from '../assets/images/cafe.webp';
 import { User, MapPin } from '@phosphor-icons/react';
 import GenBtn from './interactions/GenBtn';
 
-export default function CourseCard() {
+export default function CourseCard({ course }) {
   return (
     <div className='w-[80%] mx-auto my-52 md:max-w-[800px] '>
       <div className="flex flex-col md:flex-row">
         <div className="">
-          <img src={courseIMG} alt="Kursus Billede" className='w-[100%] md:h-[100%] md:object-cover' />
+          <img src={course.courseImgUrl} alt="Kursus Billede" className='w-[100%] md:h-[100%] md:object-cover' />
         </div>
         <div className=" bg-prime-brown text-off-white flex flex-col gap-4">
-          <h2 className='font-bebas text-4xl w-[80%] mx-auto mt-5 md:text-5xl'>Course Name</h2>
-          <p className='font-mont text-sm w-[80%] mx-auto mb-5'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam cupiditate optio alias repudiandae possimus fugit, soluta quod atque repellat laboriosam ratione, sit veniam. Eos corporis voluptates rem ea fugit consequuntur deleniti modi et, accusamus ullam saepe, impedit nihil minima illo enim, rerum debitis ut repellat? Molestias quibusdam repellat eligendi qui perspiciatis in est. Earum mollitia reprehenderit laborum perspiciatis, libero ad ex aperiam sapiente quos laudantium quae illo ipsum fuga optio exercitationem velit assumenda eum nisi distinctio! Libero accusamus porro, quis minima dolor laborum unde repellat est maiores, provident ducimus blanditiis aliquid quod quas obcaecati harum temporibus amet, quaerat eveniet quia.</p>
+          <h2 className='font-bebas text-4xl w-[80%] mx-auto mt-5 md:text-5xl'>{course.courseName}</h2>
+          <p className='font-mont text-sm w-[80%] mx-auto mb-5'>{course.courseIntroDes}</p>
         </div>
       </div>
       <div className=" font-mont text-sm border-solid border-2 border-prime-orange p-1 sm:py-4 sm:px-2 ">
         <div className="grid grid-cols-2 items-center gap-3 w-[90%] mx-auto sm:flex sm:justify-between">
-          <div className=" ">01-12-2023</div>
+          <div className=" ">{course.CourseDate}</div>
           <div className="flex items-center justify-end">
-            <div className="">12 / 30</div>
+            <div className="">{course.courseNumOfPart}</div>
             <User size={28} weight="light" color='#DB6439' />
           </div>
           <div className="flex items-center">
-            <div className="">Mindet 4D</div>
+            <div className="">{course.courseLocation}</div>
             <MapPin size={28} weight="light" color='#DB6439' />
           </div>
           <div className="flex justify-end">
---
+++ src/pages/SpecificCoursePage.jsx
@@ -0,0 +1,137 @@
+import CourseCard from "../components/CourseCard";
+import Accordion from "../components/Accordion";
+import { Plus, Minus } from "@phosphor-icons/react";
+import Placeholder from "../assets/images/pizzaplaceholder.svg";
+import SlideEmail from "../components/interactions/SlideEmail";
+
+import { useState } from "react";
+
+
+export default function SpecificCoursePage() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  }
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
+  
+
+  return (
+    <main className="bg-background w-[80%] lg:w-full flex flex-col mx-auto ">
+      <div className="flex flex-col mx-auto gap-6">
+        <div className="flex lg:flex-row  flex-col-reverse gap-3  mx-auto ">
+
+
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-3">
+              <h1 className="font-bebas text-prime-orange text-3xl  md:mt-5  md:text-4xl lg:text-5xl ">Course Name</h1>
+              <div>
+                <h2 className="font-bebas text-prime-brown text-2xl md:text-3xl lg:text-4xl">PRICE</h2>
+                <p className=" lg:w-1/2 font-mont text-xs md:text-base w-[90%]">Pizzaen er et godt eksempel på en ret, der kan varieres i det uendelige, når først man har styr på bunde, bagning og et par grundprincipper for at komponere en velsmagende pizza. Det og lidt til lærer du på dette kursus, hvor vi bager forrygende pizzaer med gode ingredienser. Dette kursus egner sig til både børn og voksne.</p>
+              </div>
+            </div>
+            <div className="flex lg:flex-row gap-32 ">
+              <h2 className="font-bebas  text-off-black text-2xl md:text-3xl lg:text-4xl">DATE</h2>
+              <input className="border border-solid rounded border-off-black" type="date" />
+            </div>
+            <div className="flex flex-col gap-6">
+
+              <div className="flex flex-row w-max gap-[50px]">
+
+                <h2 className="font-bebas text-off-black text-2xl md:text-3xl lg:text-4xl w-[90%]">VÆLG ANTAL PERSONER</h2>
+                <div className=" flex flex-row w-max gap-2 ">
+                  <button  className="w-6 h-6 bg-[#3d3030] rounded-full flex items-center justify-center cursor-pointer text-[#fbfbf4]" onClick={decrement}><Minus size={22} weight="bold" /></button>
+                  <p className="lg:w-7 lg:h-10 text-center w-6 h-6 border border-solid border-off-black rounded">{count}</p>
+                  <button  className=" w-6 h-6 bg-[#3d3030] rounded-full flex items-center justify-center cursor-pointer text-[#fbfbf4]" onClick={increment}><Plus size={22} weight="bold" /></button>
+                </div>
+              </div>
+             
+              <hr className="bg-off-black h-[2px]"  />
+              <SlideEmail />
+
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+  <img
+    className="object-cover w-full h-[300px] lg:w-1/2 mt-7 lg:h-[350px]"
+    src={Placeholder}
+    alt=""
+  />
+</div>
+        </div>
+        <div>
+          <h2 className="font-bebas  text-off-black text-2xl md:text-3xl lg:text-4xl">Hvad lærer du?</h2>
+         <div className="flex flex-col gap-5">
+          <p className="font-mont text-xs md:text-base w-[90%]">
+            Vi starter med dejen og de ingredienser og den håndtering, der giver en smidig pizzadej; en dej, der både smager af noget og kan rulles helt tyndt ud, så kanterne kan bages sprøde.
+            </p>
+           <p className="font-mont text-xs md:text-base w-[90%]"> Vi viser, hvordan du laver en enkel, lækker grundtomatsauce og giver inspiration til smagssammensætninger. Du får også mulighed for at ’freestyle’, inden vi bager vores kreationer ved de magiske +300°. Vi gennemgår selvfølgelig også, hvordan du derhjemme bedst kan nærme dig den bagning, som er mulig i en rigtig pizzaovn.</p>
+
+
+           <p className="font-mont text-xs md:text-base w-[90%]">I løbet af dagen sætter vi os til bordet og nyder resultatet af vores indsats med et godt glas vin eller æblemost.</p> 
+          
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h2 className="font-bebas  text-off-black text-2xl md:text-3xl lg:text-4xl">Hvad får du?</h2>
+          <div className="flex flex-col gap-5">
+          <li className="font-mont text-xs md:text-base w-[90%] ">
+            Du lærer at bage forrygende pizzaer med tynde, sprøde bunde og velafstemt fyld</li>
+
+           <li className="font-mont text-xs md:text-base w-[90%]">Masser af inspiration til variationsmuligheder – du får dagens opskrifter med hjem</li> 
+
+           <li className="font-mont text-xs md:text-base w-[90%]">Et lækkert måltid ledsaget af god vin, øl og læskende æblemost + 1 pizza med hjem</li>
+
+           </div>
+          
+        </div>
+        <div className="flex flex-col gap-2 ">
+          <h2 className="font-bebas  text-off-black text-2xl md:text-3xl lg:text-4xl ">Location</h2>
+          <p className="font-mont text-xs md:text-base ">
+          Mindet 4D, 8000 Aarhus C
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+
+
+          <h2 className="font-bebas  text-off-black text-2xl md:text-3xl lg:text-4xl ">Praktisk information</h2>
+          <p className="font-mont text-xs md:text-base w-[90%] ">
+            På kurset inddeles I i mindre grupper, så I kan samarbejde om at komme i mål med retterne. I bliver så vidt muligt sat på hold sammen, hvis I er flere der har booket kurset sammen.
+
+
+          </p>
+        </div>
+        <div className="h-[70vh] w-[100%] mx-auto ">
+        <Accordion
+          title="Parking"
+          content="Lorem ipsum text about the Parking component"
+          foldIcon={<Plus color="#db6439" size={24} />}
+        />
+
+        <Accordion
+          title="Ombooking"
+          content="Lorem ipsum text about the Ombooking component"
+          foldIcon={<Plus color="#db6439" size={24} />}
+        />
+        <Accordion
+          title="Hvad skal du medbringe?"
+          content="Lorem ipsum text about the Hvad skal du medbringe? component"
+          foldIcon={<Plus color="#db6439" size={24} />}
+        />
+      </div>
+      </div>
+
+      <CourseCard />
+      
+
+
+
+        </main>
+
+
+  )
+}
