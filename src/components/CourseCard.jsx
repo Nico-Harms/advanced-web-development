2720ba741334b3c0cbe77c08cf3697ca06ea7ef7
madsnbkristensen <madskristensen@hotmail.dk>
2023-12-06 11:33:47 +0100

    Update CourseCard.jsx
    NumOfPers boooking

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -2,15 +2,52 @@ import courseIMG from '../assets/images/cafe.webp';
 import { User, MapPin } from '@phosphor-icons/react';
 import GenBtn from './interactions/GenBtn';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import FirebaseApp from '../../firebaseConfig';
 
 export default function CourseCard({ course }) {
-
+  const [bookings, setBookings] = useState([]);
   const navigate = useNavigate();
+  const today = new Date();
+  const todayString = today.toISOString();
+  const [numOfBookings, setNumOfBookings] = useState(0);
+  const [totalNumOfPers, setTotalNumOfPers] = useState(0);
 
   function handleClick() {
     navigate(`/kurser/${course.id}`);
   }
-  
+
+  useEffect(() => {
+    async function getBookings() {
+      const db = getFirestore(FirebaseApp);
+      const bookingsCollection = collection(db, 'bookings');
+      const querySnapshot = await getDocs(bookingsCollection);
+
+      const bookingsArray = [];
+      querySnapshot.forEach((doc) => {
+        bookingsArray.push({ id: doc.id, ...doc.data() });
+      });
+
+      const currentBookings = bookingsArray.filter((booking) => booking.courseDate > todayString);
+      setBookings(currentBookings);
+
+      const numOfBookingsForCourse = currentBookings.filter((booking) => booking.courseName === course.courseName).length;
+      setNumOfBookings(numOfBookingsForCourse);
+
+      const totalNumOfPersForCourse = currentBookings.reduce((total, booking) => {
+        if (booking.courseName === course.courseName) {
+          return total + booking.numOfPers;
+        }
+        return total;
+      }, 0);
+
+      setTotalNumOfPers(totalNumOfPersForCourse);
+    }
+
+    getBookings();
+  }, [course.courseName, todayString]);
+
   return (
     <div className='w-[80%] mx-auto my-52 md:max-w-[800px] '>
       <div className="flex flex-col md:grid md:grid-cols-[1fr,2fr]">
@@ -26,7 +63,7 @@ export default function CourseCard({ course }) {
         <div className="grid grid-cols-2 items-center gap-3 w-[90%] mx-auto sm:flex sm:justify-between">
           <div className=" ">{course.courseDate}</div>
           <div className="flex items-center justify-end">
-            <div className="">{course.courseNumOfPart}</div>
+            <div className=""> {totalNumOfPers} / {course.courseNumOfPart}</div>
             <User size={28} weight="light" color='#DB6439' />
           </div>
           <div className="flex items-center">
@@ -39,5 +76,5 @@ export default function CourseCard({ course }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
