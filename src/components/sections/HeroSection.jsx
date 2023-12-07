8e404678757670d9ee4b447220cefe49c72ce087
TobiasRW <tobiasrw98@gmail.com>
2023-12-07 12:11:57 +0100

    paths

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -2,8 +2,7 @@ import GenBtn from "../interactions/GenBtn";
 import heroVideo from "../../assets/videos/heroVid.mp4";
 import React, { useEffect, useState } from 'react';
 import '../interactions/css/TextSlider.css';
-import { comma } from "postcss/lib/list";
-import { comment } from "postcss";
+import { useNavigate } from "react-router-dom";
 
 
 /*===============================================
@@ -12,6 +11,9 @@ import { comment } from "postcss";
 
 
 export default function HeroSection() {
+
+  const navigate = useNavigate();
+
   const [activeDot, setActiveDot] = useState(0);
   /*===============================================
   =          Text inputs for sliders          =
@@ -23,7 +25,7 @@ export default function HeroSection() {
       <hr className="w-1/4 border-solid border-1 border-black md:w-[80%]" />
       <p className="font-mont py-3 text-sm md:py-2 md:text-sm lg:text-1xl xl:text-2xl md:w-[90%] 2xl:text-3xl">Som tidligere madskole, lægger vi meget vægt på at lære fra os, og derfor betyder vores kurser rigtigt meget for os. Gå ikke glip
         af muligheden, og gå ind og check vores kommende kurser ud!</p>
-      <GenBtn content="Se kurser" btnType="primaryBtn" />
+      <GenBtn content="Se kurser" btnType="primaryBtn" click={() => navigate('/kurser')} />
     </div>
   );
 
@@ -33,7 +35,7 @@ export default function HeroSection() {
       <hr className="w-1/4  border-solid border-1 border-black md:w-[80%]" />
       <p className="font-mont py-3 text-sm md:py-2 md:text-sm lg:text-1xl xl:text-2xl md:w-[90%] 2xl:text-3xl">Jumbo har ikke bare altid været Jumbo, som mange andre steder har vi gået en større forvandling for at nå til hvor vi er idag.
         Vi vil gerne dele den historie med jer.</p>
-      <GenBtn content="Læs mere" btnType="primaryBtn" />
+      <GenBtn content="Læs mere" btnType="primaryBtn" click={() => navigate('/historie')} />
     </div>
   );
 
@@ -42,7 +44,7 @@ export default function HeroSection() {
       <h2 className="text-prime-orange text-3xl font-bebas mt-7 lg:text-[4rem] md:text-[2.5rem] ">Kontakt os</h2>
       <hr className="w-1/4 border-solid border-1 border-black md:w-[80%]" />
       <p className="font-mont py-3 text-sm md:py-2 md:text-sm lg:text-1xl xl:text-2xl md:w-[90%] 2xl:text-3xl">Hvis du har nogle spørgsmål, vil søge job eller bare gerne vil i kontakt med os, så find vores oplysninger her.</p>
-      <GenBtn content="Se mere" btnType="primaryBtn" />
+      <GenBtn content="Se mere" btnType="primaryBtn" click={() => navigate('/contact')} />
     </div>
   );
 
